5ebd1bd8ca0b22892e6af377aea92b6c4ad035e8
ZhuYide <1046787953@qq.com>
2024-04-02 15:35:41 +0800

    add one blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,32 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), react()],
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://shiki.style/themes
-      theme: 'vitesse-dark',
-      // Alternatively, provide multiple themes
-      // https://shiki.style/guide/dual-themes
-      //  themes: {
-      //     light: 'github-light',
-      //     dark: 'github-dark',
-      //  },
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://shiki.style/languages
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-      // Add custom transformers: https://shiki.style/guide/transformers
-      // Find common transformers: https://shiki.style/packages/transformers
-      transformers: []
-    }
-  }
-});
+   integrations: [tailwind(), mdx(), react()],
+   markdown: {
+      shikiConfig: {
+         // Choose from Shiki's built-in themes (or add your own)
+         // https://shiki.style/themes
+         theme: 'material-theme',
+         // Alternatively, provide multiple themes
+         // https://shiki.style/guide/dual-themes
+         //  themes: {
+         //     light: 'github-light',
+         //     dark: 'github-dark',
+         //  },
+         // Add custom languages
+         // Note: Shiki has countless langs built-in, including .astro!
+         // https://shiki.style/languages
+         langs: [],
+         // Enable word wrap to prevent horizontal scrolling
+         wrap: true,
+         // Add custom transformers: https://shiki.style/guide/transformers
+         // Find common transformers: https://shiki.style/packages/transformers
+         transformers: [],
+      },
+   },
+})
